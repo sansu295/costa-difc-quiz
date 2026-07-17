83c8ad8dea36b79dc6189d4f59900f8ca5971283
sansu295 <gynhynfu@gmail.com>
2026-07-17 16:11:56 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const quizContainer = document.getElementById('quiz');
+const questionEl = document.getElementById('question');
+
+// Simple test to see if it loads
+document.addEventListener('DOMContentLoaded', () => {
+    questionEl.innerText = "Welcome to the Coffee Quiz!";
+});
